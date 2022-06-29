620708eed7de2133bf8d7dbf2e2859e595ddf51e
palachini <mateuspalachini@gmail.com>
2022-06-29 19:57:07 -0400

    update react-dom 17 from 18, concept state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
+  return (
+  <>
+    <RepositoryList />
+    <Counter/>
+  </>
+  )
 
-  return <h1>Hello ReactJS</h1>
 }
